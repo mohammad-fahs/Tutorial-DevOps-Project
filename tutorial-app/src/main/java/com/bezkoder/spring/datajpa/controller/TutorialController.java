6df9064d99bfa8dd937a5cfd19e5a890bfaeadb5
Mohamad fahs <109221274+mohammad-fahs@users.noreply.github.com>
2023-12-20 01:06:31 +0200

    asd

--- tutorial-app/src/main/java/com/bezkoder/spring/datajpa/controller/TutorialController.java
+++ tutorial-app/src/main/java/com/bezkoder/spring/datajpa/controller/TutorialController.java
@@ -29,7 +29,7 @@ public class TutorialController {
 	@Autowired
 	TutorialRepository tutorialRepository;
 
-	//this is test updated
+	//check if it will trigger
 	@GetMapping("/tutorials")
 	public ResponseEntity<List<Tutorial>> getAllTutorials(@RequestParam(required = false) String title) {
 		try {
