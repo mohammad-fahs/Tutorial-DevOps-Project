9c0af1612e3597446b1b44d5f4ddd53213dd6e7e
Mohamad fahs <109221274+mohammad-fahs@users.noreply.github.com>
2023-12-21 11:38:37 +0200

    test

--- test-app/api.test.js
+++ test-app/api.test.js
@@ -12,7 +12,7 @@ const baseURL = 'http://app:6868/api';
 // Test for the POST request
 describe('POST /tutorials', () => {
   it('should create a new tutorial', async () => {
-    // Define the request payload
+    // Define the request payload this is test
     const data = {
       title: 'first tutorial',
       description: 'this is the description of the first tutorial',
